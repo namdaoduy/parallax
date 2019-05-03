9b64fb6ce84de85a9b45124f70d0032a4a656ef9
nam-hackintosh <lenkc98@gmail.com>
2019-05-04 01:15:19 +0700

    Add animation. Bug fix

--- scripts.js
+++ scripts.js
@@ -1,2 +1,12 @@
-var scene = document.getElementById('scene');
-var parallaxInstance = new Parallax(scene);
+(function(){
+  const scene = document.getElementById('scene');
+  const parallaxInstance = new Parallax(scene);
+
+  const overlay = document.getElementsByClassName('overlay')[0];
+  window.onload = () => {
+    overlay.classList.add('faded');
+    setTimeout(() => {
+      overlay.parentNode.removeChild(overlay);
+    }, 2000);
+  }
+})();
